63ddb421ba450eb286ead6a1f8397a0abcd19af8
Laura Damaceno de Almeida <laura.damaceno@ibm.com>
2018-12-27 15:14:49 -0200

    Acrescimo de metodos, e solucao do problema no for da main

--- main.go
+++ main.go
@@ -5,41 +5,76 @@ import "os"
 import "net/http"
 
 func main(){
+	for{
 	exibeMenu()
-	entrada := lerEntrada()
-	validarEntrada(entrada)
+	lerEntrada()
+	}
 }
-
-func validarEntrada(entrada int){
+func validaEntrada(entrada int){
 	switch entrada {
 	case 1:
 		iniciarMonitoramento()
+		break
 	case 2:
 		fmt.Println("Opcao 2")
-	case 0:
+	case 9:
 		fmt.Println("Adeus...")
 		os.Exit(0)
 	default:
 		fmt.Println("Opsss!!! Nao entendi este comando")
-		os.Exit(-1)
 	}
 
-}
-func  lerEntrada()int{
-	var comando int
-	fmt.Scanf("%d", &comando)
-	return comando
 
 }
+
+func lerEntrada(){
+	var comando int
+	fmt.Scanf("%d", &comando)
+	fmt.Println(comando)
+	validaEntrada(comando)
+}
+
 func exibeMenu(){
 	fmt.Println("Digite uma opcao:")
-	fmt.Println("1- Monitoramento \n2- Exibir logs \n0- Sair")
+	fmt.Println("1- Monitoramento \n2- Exibir logs \n9- Sair")
 }
+
 //fazer requisições HTTP, para fazer o monitoramento do site escolhido
 func iniciarMonitoramento(){
 	fmt.Println("Monitorando....")
-	site := "https://www.meetup.com/pt-BR/bluetalks/events/"
-	resp,_ := http.Get(site)
-	fmt.Println(resp)
+	site := escolhaDoSite(getSites())
+	testaSite(site)
 
+}
+
+func testaSite(site string){
+	resp,_ := http.Get(site)
+	if resp.StatusCode == 200{
+		fmt.Println("Site",site,"foi carregado com sucesso")
+	}else{
+		fmt.Println("Site ",site)
+		fmt.Println("Chama o SAMUUU  \nStatus Code",resp.StatusCode)
+	}
+}
+
+func escolhaDoSite(sites[]string)string{
+
+	for i,site:=range sites{
+		fmt.Println(i,site)
+		return site
+	}
+	return "https://random-status-code.herokuapp.com/"
+}
+
+func getSites()[]string{
+	sites := []string{"https://random-status-code.herokuapp.com/","https://www.meetup.com/pt-BR/bluetalks/events","https://www.google.com/"}
+	return sites
+}
+
+func setSites(){
+	var site string
+	fmt.Println("Digite o novo site")
+	fmt.Scanf("%s", &site)
+	fmt.Println(site)
+	//sites = append(sites,site)
 }
