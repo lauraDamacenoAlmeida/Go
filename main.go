cb9e108a69345a7423c66ff45b7f2c62fb8d6c71
Laura Damaceno de Almeida <laura.damaceno@ibm.com>
2019-01-02 15:43:33 -0200

    Final do curso de GO, leitura e escrita de arquivos

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import(
 	"os"
 	"net/http"
 	"time"
+	"bufio"
+	"io"
+	"strings"
+	"io/ioutil"
 )
-const monitoramentos = 3
 const delay = 5
 
 func main(){
+	registraLog("www.google.com",false)
 	for{
 	exibeMenu()
 	lerEntrada()
@@ -21,7 +25,7 @@ func validaEntrada(entrada int){
 		iniciarMonitoramento()
 		break
 	case 2:
-		fmt.Println("Opcao 2")
+		imprimeLogs()
 	case 9:
 		fmt.Println("Adeus...")
 		os.Exit(0)
@@ -47,37 +51,73 @@ func exibeMenu(){
 //fazer requisições HTTP, para fazer o monitoramento do site escolhido e define o tempo para testar
 func iniciarMonitoramento(){
 	fmt.Println("Monitorando....")
-
-	for i:=0; i<monitoramentos;i++{
-	site := escolhaDoSite(i,getSites())
-	fmt.Println("Testando o site ",site)
-	testaSite(site)
+	site := lerSites()
+	for i:=0; i<len(site);i++{
+	fmt.Println("Testando o site ",site[i])
+	testaSite(site[i])
 	time.Sleep(delay * time.Second)
 	}
 }
 
 func testaSite(site string){
-	resp,_ := http.Get(site)
+	resp,err := http.Get(site)
+	if err != nil{
+		fmt.Println("Ops!! Ocorreu um erro: ",err)
+	}
 	if resp.StatusCode == 200{
 		fmt.Println("Site foi carregado com sucesso")
+		registraLog(site,true)
 	}else{
 		fmt.Println("Chama o SAMUUU  \nStatus Code",resp.StatusCode)
+		registraLog(site,false)
 	}
 }
 
-func escolhaDoSite(i int,sites[]string)string{
-	return sites[i]
-}
 
 func getSites()[]string{
-	sites := []string{"https://random-status-code.herokuapp.com/","https://www.meetup.com/pt-BR/bluetalks/events","https://www.google.com/"}
+	//sites := []string{"https://random-status-code.herokuapp.com/","https://www.meetup.com/pt-BR/bluetalks/events","https://www.google.com/"}
+	sites := lerSites()
 	return sites
 }
+func lerSites()[]string{
+	var sites [] string
+	arquivo, err := os.Open("sites.txt")
+	if err != nil{
+		fmt.Println("Ops!! Ocorreu um erro: ",err)
+	}
+	leitor := bufio.NewReader(arquivo)
+	for{
+	linha,err:= leitor.ReadString('\n')
+	linha = strings.TrimSpace(linha)
+	if err == io.EOF{
+		break;
+	}
+	sites = append(sites,linha)
+	}
 
-func setSites(){
-	var site string
-	fmt.Println("Digite o novo site")
-	fmt.Scanf("%s", &site)
-	fmt.Println(site)
-	//sites = append(sites,site)
+	arquivo.Close()
+	return sites
+}
+func registraLog(site string, status bool){
+	arq,err := os.OpenFile("logs.txt",os.O_RDWR| os.O_CREATE| os.O_APPEND,0666)
+	if err != nil{
+		fmt.Println(err)
+	}
+	online := "No ar"
+	if(status != true){
+		online = "Site está fora do ar"
+	}
+	//para representar os dias e as horas atuais, no GO eh feito através dos numeros especificos
+	arq.WriteString(time.Now().Format("02/01/2006 15:04:05")+" - Site: "+ site + " - status: " + online +"\n")
+	arq.Close()
+}
+
+func imprimeLogs(){
+	arq,err := ioutil.ReadFile("logs.txt")
+
+	if err != nil{
+		fmt.Println("OPSS, arquivo nao encontrado, favor verificar")
+	}
+	fmt.Println(string(arq))
+
 }
